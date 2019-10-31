6c973dfb948b7023a379e3b4cc3e776f3fb91f5e
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-10-31 17:10:19 -0600

    js2-1 first commit

--- practice.js
+++ practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Steven',
+  age: 31
+}
+
+// alert(me.name)
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +28,31 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'odesza',
+  food: 'pizza',
+  person: 'kirstyn',
+  book: 'born to run',
+  movie: 'interstellar',
+  holiday: 'fourth of july'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'mini van';
+favoriteThings.brand = 'van brand';
+
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +64,18 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+let backPack = {
 
+};
+
+let item = 'firstPocket';
+
+backPack['firstPocket'] = 'chapstick'
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backpack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -84,7 +104,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in";
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
